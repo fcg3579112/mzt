a180def047d864f173a4a8fa0f81d70f0c942718
冯传高 <chuangao.feng@inin88.com>
2018-08-01 18:03:01 +0800

    Feat: 添加抓取 tile 逻辑

--- mzt/spiders/mz.py
+++ mzt/spiders/mz.py
@@ -1,4 +1,5 @@
 import scrapy
+import os.path
 
 class MZSpider(scrapy.Spider):
 
@@ -13,4 +14,15 @@ class MZSpider(scrapy.Spider):
 
     def parse(self, response):
     	list = response.xpath('//div[@class="postlist"]//a[@href]').re('http://www.mzitu.com/\d{5,6}')
-    	print(list)
+    	for url in list:
+    		yield scrapy.Request(url=url, callback=self.parseItemInfo ,dont_filter=True)
+    		break
+
+    def parseItemInfo(self, response):
+    	max_page = response.xpath('//div[@class="pagenavi"]//span/text()')[-2].extract()
+    	img = response.xpath('//div[@class="main-image"]//a/img')
+    	url = img.xpath('@src').extract_first()
+    	title = img.xpath('@alt').extract_first()
+    	components = url.split('/')
+    	print(title)
+    	print(os.getcwd())
