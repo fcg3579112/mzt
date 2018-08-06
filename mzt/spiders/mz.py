22ed2028bb34f0fafc4c4268d94d61461edf2318
feng <feng@fengs-iMac.local>
2018-08-07 07:04:09 +0800

    Feat: 优化存储逻辑

--- mzt/spiders/mz.py
+++ mzt/spiders/mz.py
@@ -1,58 +1,62 @@
 import scrapy
 import os.path
-
+file_dir = "pics"
 class MZSpider(scrapy.Spider):
+    
 
     name = "mzt"
+    
 
     def start_requests(self):
-        urls = [
-            'http://mzitu.com/xinggan/2',
-        ]
+        urls = list()
+        for page in range(1,100):
+            urls.append('http://mzitu.com/xinggan/page/' + str(page))
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse ,dont_filter=True)
 
     def parse(self, response):
-    	list = response.xpath('//div[@class="postlist"]//a[@href]').re('http://www.mzitu.com/\d{6}')
-    	for url in list:
-    		yield scrapy.Request(url=url, callback=self.parseItemInfo ,dont_filter=True)
-    		break
-
+        urls = response.xpath('//div[@class="postlist"]//li/a/@href').extract()
+        titles = response.xpath('//div[@class="postlist"]//li//img/@alt').extract()
+        for (url,title) in zip(urls,titles):
+            yield scrapy.Request(url=url, callback=self.parseItemInfo ,meta={'title':title},dont_filter=True)
+        
     def parseItemInfo(self, response):
     	max_page = response.xpath('//div[@class="pagenavi"]//span/text()')[-2].extract()
-    	img = response.xpath('//div[@class="main-image"]//a/img')
-    	title = img.xpath('@alt').extract_first()
+    	title = response.meta['title'].replace(' ','_')
     	self.makeDir(title)
     	for page in range(1,int(max_page) + 1):
     		url = response.url
     		if page != 1:
 	    		url = response.url + '/' + str(page)
-    		yield scrapy.Request(url=url, callback=self.parsePicUrl, dont_filter=True)
-    		break
-
+    		yield scrapy.Request(url=url, callback=self.parsePicUrl, meta={'title':title},dont_filter=True)
+    		
     def makeDir(self, file_name):
-    	dir = os.path.join(os.getcwd(),"pics",file_name)
+    	dir = os.path.join(os.getcwd(),file_dir,file_name)
     	if not os.path.exists(dir):
     		os.makedirs(dir)
     	
     def parsePicUrl(self, response):
     	referer = response.url
-    	img = response.xpath('//div[@class="main-image"]//a/img')
-    	title = img.xpath('@alt').extract_first()
-    	url = img.xpath('@src').extract_first()
-    	meta = {'title':title}
+    	url = response.xpath('//div[@class="main-image"]//a/img/@src').extract_first()
+    	meta = {'title':response.meta['title']}
     	yield scrapy.Request(url=url, callback=self.parsePicInfo,meta=meta, dont_filter=True)
 
     def parsePicInfo(self, response):
     	self.writeFile(response.body,response.url,response.meta['title'])
     	
-
     def writeFile(self,pic_data,url,title):
-    	print(title)
-    	filename = url.split('/')
-    	filename = filename[-3] + filename[-2] + filename[-1]
-    	file_path = os.path.join(os.getcwd(),'pics',title,filename)
-    	with open(file_path, 'wb') as f:
-    		f.write(pic_data)
+        print(title)
+        filename = url.split('/')
+        filename = filename[-3] + '_' + filename[-2] + '_' + filename[-1]
+        text_file_path = os.path.join(os.getcwd(),file_dir,title,title + '.text')
+        file_path = os.path.join(os.getcwd(),file_dir,title,filename)
+
+        with open(text_file_path,'a') as fm:
+            fm.write('\n' + url)
+            fm.close()
+        with open(file_path, 'wb') as f:
+            f.write(pic_data)
+            f.close()
 
     	
